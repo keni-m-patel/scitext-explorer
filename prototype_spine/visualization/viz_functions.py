b05d6e5f84b7439274427a9fcd4831f34fe6ffd1
Sonja Lindberg <sonja.c.lindberg@gmail.com>
2018-06-22 15:15:04 -0400

    *dab dab dab* we got an end to end, wordcloud fixed and can end by closing window

--- prototype_spine/visualization/viz_functions.py
+++ prototype_spine/visualization/viz_functions.py
@@ -1,20 +1,37 @@
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+from random import shuffle
+
+# def bow_to_fs(bow_dict):
+#     fs = ''
+#     for word,freq in bow_dict.items():
+#         for i in range(freq):
+#             fs = fs + ' ' + word
+#     return fs
+
 def bow_to_fs(bow_dict):
-    fs = ''
+    word_list = []
     for word,freq in bow_dict.items():
         for i in range(freq):
-            fs = fs + word + ' '
+            word_list.append(word)
+    print('word_list: \n', word_list)
+
+    shuffle(word_list)
+    shuffled_word_list = word_list
+
+    fs = ''
+    for w in shuffled_word_list:
+        fs = fs + ' ' + w
     return fs
 
 def bow_to_wordcloud(bow):
     print('bow:', bow)
 
     docs = bow_to_fs(bow)
-    print('docs:', docs)
+    print('\n fs docs: \n', docs)
     wc = WordCloud().generate(docs)
-    plt.imshow(wc, interpolation ='bilinear')
+    plt.imshow(wc, interpolation='bilinear')
     plt.axis('off')
     wc = WordCloud(max_font_size=40).generate(docs)
     return plt.show()
