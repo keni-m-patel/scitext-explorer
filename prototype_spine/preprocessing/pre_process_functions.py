91643934b82e82146bd871c68b85a2c070308032
twalsh97 <twalsh5@nd.edu>
2018-06-22 10:19:26 -0400

    Quick change to preprocess fun

--- prototype_spine/preprocessing/pre_process_functions.py
+++ prototype_spine/preprocessing/pre_process_functions.py
@@ -80,11 +80,10 @@ def complete_tokenize(documents):
 
 def stem_tokens(documents):  
     ps = PorterStemmer()
-    stem_words=list()
     stem_docs=list()
     for tokens in documents:
         for item in tokens:
-            stem_words.append(ps.stem(item))
+            stem_docs.append(ps.stem(item))
     return stem_docs
 
 
