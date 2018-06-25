5215e7a900126277a1b06e5b960904fb688a51ed
Sonja Lindberg <sonja.c.lindberg@gmail.com>
2018-06-25 13:16:45 -0400

    moved main logic into main_logic and fixed wordcloud for muliple docs & show both figures

--- prototype_spine/visualization/viz_functions.py
+++ prototype_spine/visualization/viz_functions.py
@@ -1,21 +1,19 @@
+DEBUG = 0
+
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-
+from matplotlib import interactive
 from random import shuffle
 
-# def bow_to_fs(bow_dict):
-#     fs = ''
-#     for word,freq in bow_dict.items():
-#         for i in range(freq):
-#             fs = fs + ' ' + word
-#     return fs
 
 def bow_to_fs(bow_dict):
     word_list = []
     for word,freq in bow_dict.items():
         for i in range(freq):
             word_list.append(word)
-    print('word_list: \n', word_list)
+
+    if DEBUG:
+        print('\n\n\nword_list: \n', word_list)
 
     shuffle(word_list)
     shuffled_word_list = word_list
@@ -25,15 +23,22 @@ def bow_to_fs(bow_dict):
         fs = fs + ' ' + w
     return fs
 
-def bow_to_wordcloud(bow):
-    print('bow:', bow)
 
-    docs = bow_to_fs(bow)
-    print('\n fs docs: \n', docs)
-    wc = WordCloud().generate(docs)
-    plt.imshow(wc, interpolation='bilinear')
-    plt.axis('off')
-    wc = WordCloud(max_font_size=40).generate(docs)
+def multiple_bow_to_wordcloud(list_of_bow):
+    for i in range(len(list_of_bow)): 
+        bow = list_of_bow[i]
+        docs = bow_to_fs(bow)
+
+        if DEBUG:
+            print('\n\n\n bow: \n', bow)
+            print('\n\n\n fs docs: \n', docs)
+        plt.figure(i)
+        wc = WordCloud().generate(docs)
+        plt.imshow(wc, interpolation='bilinear')
+        plt.axis('off')
+        wc = WordCloud(max_font_size=40).generate(docs)
     return plt.show()
+
+
 
 
