b6a340a34c70f474185429d71010fa7fc68c5ad2
twalsh97 <twalsh5@nd.edu>
2018-07-11 12:43:07 -0400

    added options for preprocess

--- preprocess.py
+++ preprocess.py
@@ -44,13 +44,23 @@ class Preprocessor(object):
             self.stop = list(set(self.stop) - set(self.config['remove_stop_list']))
         
         tokens = word_tokenize(self.corpus)
+        
         if self.config['lemmatize']:  
             tokens = pos_tag(tokens)
-            tokens = [t[0].lower() for t in tokens]              
-            tokens = [t for t in tokens if t[0].isalpha() and t[0] not in self.stop]
+            if self.config['lowercase']:
+                tokens = [t[0].lower() for t in tokens]
+            if self.config['alpha_only']:
+                tokens = [t for t in tokens if t[0].isalpha()]   #and t[0] not in self.stop]
+            if self.config['use_stop_list']:
+                tokens = [t for t in tokens if t[0] not in self.stop]
+                
         else:
-            tokens = [t.lower() for t in tokens]              
-            tokens = [t for t in tokens if t.isalpha() and t not in self.stop]   
+            if self.config['lowercase']:
+                tokens = [t.lower() for t in tokens]    
+            if self.config['alpha_only']:
+                tokens = [t for t in tokens if t.isalpha()]  #and t not in self.stop]   
+            if self.config['use_stop_list']:
+                tokens = [t for t in tokens if t not in self.stop]
          
              
         self.token_list = []
