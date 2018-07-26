dd40a635d271f7fd4f3133af5171dd6ebbc0c8c9
twalsh97 <40073618+twalsh97@users.noreply.github.com>
2018-07-26 11:51:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,23 +21,6 @@ print('\n\nreading from the following configuration files: \n\n ', utilities.get
 
 corpus_list = [Corpus(config_file) for config_file in utilities.get_config('./config/data/master.yaml')['config_files']]
 corpi = Merge([corpus() for corpus in corpus_list])
-    
-#corpus_doc_name_lists = [corpus.get_file_names() for corpus in corpus_list]  # corpus.get_file_names()
-#doc_names = []
-#for c_list in corpus_doc_name_lists:
-#    doc_names += c_list
-
-#print('doc_names\n\n', doc_names)
-#print(len(doc_names))
-
-
-
-# token_dict = dict(zip(doc_names, chain(tokenized_docs, repeat(None))))
-
-
-# print(file_object)
-# print(file_object.grouping)
-# print(file_object.config)
 
 
 alg = Algorithm(corpi, './config/algorithms.yaml')
