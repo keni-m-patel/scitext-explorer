9c648c07e04f2c3a0bd86b14c7d07949f47e00c5
twalsh97 <twalsh5@nd.edu>
2018-07-31 14:11:19 -0400

    list comp in LDA

--- algorithms.py
+++ algorithms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6a7e76e34994f030a44f1e305cb8cd30a9c1c7d8f4d6825d1c0b3986174dbee
-size 13850
+oid sha256:26a63b67e562532d565c1fdd6899b453fd1f52714733a0516de703bcd678404d
+size 13789
