01b4fc06d2230f56b21a9fff0ad0e1d391e17893
BGochett <40073505+BGochett@users.noreply.github.com>
2018-07-31 14:40:29 -0400

    new code

--- structures.py
+++ structures.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79684bbbec17386155524bca87eabe08aadb08032355377a075d1df7aa7a06e6
-size 12721
+oid sha256:30b85fea43b5a757a42e28424aebfa319b4f871d02ebfef790bcaed80e7b7565
+size 13286
