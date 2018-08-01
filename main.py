4f9a4c9bfe04861818ae5ee602866999f1d56a49
twalsh97 <40073618+twalsh97@users.noreply.github.com>
2018-08-01 14:28:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c58b29e6b0ee65e9baaec85907f5dcbdb036684ff7583aa94b849dbd0029d658
-size 926
+import logging
+from structures import Corpus, Merge
+from algorithms import Algorithm
+from visualization import Visualization
+import utilities
+
+
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)s %(levelname)s %(message)s',
+                    filename='scitext.log',
+                    filemode='w')
+'''
+TODO:
+# ADD MERGE class and functionality with master config and then allow linek to all config classes
+
+'''
+print('\n\nreading from the following configuration files: \n\n ', utilities.get_config('./config/data/master.yaml')['config_files'])
+
+corpi_list = [Corpus(config_file) for config_file in utilities.get_config('./config/data/master.yaml')['config_files']]
+corpi = Merge([corpus() for corpus in corpi_list])
+
+
+alg = Algorithm(corpi, './config/algorithms.yaml')
+
+
+alg.run()
+
+vis = Visualization( './config/visualization.yaml', './config/algorithms.yaml', alg)
+
+
+vis.run()
